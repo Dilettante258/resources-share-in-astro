0186e80fd1696000e298d5072da7be9b563e4c8a
Dilettante258 <2226519330@qq.com>
2024-01-19 14:37:56 +0800

    初始版本

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,83 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+	vite: {
+		assetsInclude: ["**/*.ppt", "**/*.pptx", "**/*.doc", "**/*.docx"]
+	  },
+	  integrations: [starlight({
+		title: 'Campus-Share-Template',
+		defaultLocale: 'root',
+		locales: {
+		  root: {
+			label: '简体中文',
+			lang: 'zh-CN'
+		  }
+		},
+		social: {
+		  github: 'https://github.com/dilettante258/campus-share-template'
+		},
+		customCss: ['./src/styles/custom.css'],
+		sidebar: [{
+		  label: '首页',
+		  link: 'homepage'
+		},
+		//指向内部或外部页面的链接
+		{
+		  label: '计算机学院',
+		  // 自动生成一个链接分组，用于 'CS' 目录。
+		  autogenerate: {
+			directory: 'CS'
+		  }
+		}, {
+		  label: '经济学院',
+		  autogenerate: {
+			directory: 'Economics'
+		  }
+		}, {
+		  label: '集成电路科学与工程学院',
+		  autogenerate: {
+			directory: 'IC'
+		  }
+		}, {
+		  label: '👉了解 Astro',
+		  link: 'https://astro.build/'
+		}],
+		head: [{
+		  tag: 'link',
+		  attrs: {
+			rel: "stylesheet",
+			href: 'https://fastly.jsdelivr.net/npm/katex@0.15.1/dist/katex.css',
+			defer: true,
+			'integrity': "sha384-WsHMgfkABRyG494OmuiNmkAOk8nhO1qE+Y6wns6v+EoNoTNxrWxYpl5ZYWFOLPCM",
+			'crossorigin': "anonymous"
+		  }
+		}, {
+		  tag: 'script',
+		  attrs: {
+			'is:inline': true,
+			src: 'https://cdn.staticfile.org/twikoo/1.6.30/twikoo.all.min.js'
+		  }
+		}],
+		expressiveCode: {
+		  styleOverrides: {
+			borderRadius: '0.5rem'
+		  }
+		},
+		components: {
+		  Footer: './src/components/Footer.astro',
+		  MarkdownContent: './src/components/MarkdownContent.astro'
+		},
+		editLink: {
+		  baseUrl: 'https://github.com/withastro/starlight/edit/main/'
+		},
+		lastUpdated: true
+	  })],
+	  markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+	  },
+	});
---
+++ src/content/docs/Economics/Economy.md
@@ -0,0 +1,17 @@
+---
+title: 经济学
+description: 经济学院开设。
+---
+
+支持Latex，见下图。
+
+$$
+\begin{aligned}dV&=\sum_{t=1}^T\frac{-C_t\cdot t}{(1+r)^{t+1}}\cdot dr=-\frac1{1+r}\sum_{t=1}^T\frac{-C_t\cdot t}{\left(1+r\right)^t}\cdot dr\\\\\frac{dV}V&=-\frac1{1+r}\cdot\frac1V\sum_{t=1}^T\frac{C_t\cdot t}{\left(1+r\right)^t}\cdot dr=-\frac1{1+r}\cdot D\cdot dr=-D_{adj}\cdot dr\end{aligned}
+$$
+
+
+### 贡献记录
+
+| 贡献者 | 时间 | 来源 | 内容 |
+|-------------------|:--------:|:--------:|:----------:|
+|   [@Dilettante258](https://github.com/Dilettante258)]    |  202X-0X  | 2X级计院 | 201X-201X-01</br>201X-201X-01</br> 201X-201X-01|
